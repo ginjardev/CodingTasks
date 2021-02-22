9507a2d656834f02149e07a81037cfb00d198a82
Iniubong Arthur <arthurshur@gmail.com>
2021-02-22 18:36:39 +0100

    Odd & Even Numbers

---
+++ EvenNumbers.java
@@ -0,0 +1,15 @@
+public class EvenNumbers{
+	public static void main(String[] args) {
+
+	int counter = 1;
+	
+	while(counter < 100) {
+	if (counter % 2 == 0) {
+	System.out.println(counter);
+}
+	counter++; 
+}
+
+}
+
+}
---
+++ OddNumbers.java
@@ -0,0 +1,15 @@
+public class OddNumbers{
+	public static void main(String[] args) {
+
+	int counter = 2;
+	
+	while(counter > 1 && counter < 100) {
+	if (counter % 2 == 1) {
+	System.out.println(counter);
+}
+	counter++; 
+}
+
+}
+
+}
